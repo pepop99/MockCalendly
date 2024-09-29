995f8436ea3bd47bc9ae679e355247a9bd4440c9
Rishabh Arora <rishabharora1780@gmail.com>
2024-09-29 18:02:06 +0530

    now checking before saving user.

--- src/main/java/com/pepop99/calendly/controller/UserController.java
+++ src/main/java/com/pepop99/calendly/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pepop99.calendly.controller;
 
 import com.pepop99.calendly.model.User;
+import com.pepop99.calendly.repository.UserRepository;
 import com.pepop99.calendly.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +15,14 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/save")
     public User save(@RequestBody User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }
         return userService.save(user);
     }
 }
